d27cc84ed1177c7a4524b9fecb168e15162b515b
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-12-05 16:02:23 +0100

    WORKING - Adding AddNewplayer but pb auto increment Id

--- Benoit_Sorting_App/Controllers/PlayerController.cs
+++ Benoit_Sorting_App/Controllers/PlayerController.cs
@@ -1,10 +1,23 @@
 ﻿using benoit_Sorting_App.Models;
+using Microsoft.AspNetCore.Mvc;
 
 
 namespace Benoit_Sorting_App.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class PlayerController
     {
         private static List<Player> players = new List<Player>();
+        [HttpPost]
+        public async Task<ActionResult<List<Player>>> AddNewPlayer(String newPlayerAlias)
+        {
+            Player newPlayer = new Player
+            {
+                Id : players.Count();
+            }
+            players.Add();
+
+        }
     }
 }
