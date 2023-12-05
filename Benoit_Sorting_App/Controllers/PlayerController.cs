5259f6cb5761e3d226b50b6d2b7ee9342640ded0
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-12-05 15:48:28 +0100

    Create empty list of players

---
+++ Benoit_Sorting_App/Controllers/PlayerController.cs
@@ -0,0 +1,10 @@
+﻿using benoit_Sorting_App.Models;
+
+
+namespace Benoit_Sorting_App.Controllers
+{
+    public class PlayerController
+    {
+        private static List<Player> players = new List<Player>();
+    }
+}
