d062fa728eb97b7289e20cb4e421d9a0fa963f12
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-12-06 15:25:46 +0100

    Setting public and private get and set in Player model

--- Benoit_Sorting_App/Models/Player.cs
+++ Benoit_Sorting_App/Models/Player.cs
@@ -4,8 +4,26 @@ namespace benoit_Sorting_App.Models
 {
     public class Player
     {
-        public int Id { get; set; }
-        public string PlayerAlias { get; set; } = string.Empty;
-        public int PlayerScore { get; set; }
+        private int _id;
+        private string _playerAlias;
+        private int _playerScore;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string PlayerAlias
+        {
+            get { return _playerAlias; }
+            set { _playerAlias = value; }
+        }
+
+        public int PlayerScore
+        {
+            get { return _playerScore; }
+            set { _playerScore = value; }
+        }
     }
 }
