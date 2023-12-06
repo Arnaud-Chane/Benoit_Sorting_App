f5fc09d09357947eceb7285ff196350bfe06e0ed
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-12-06 16:51:12 +0100

    Adding simple http delete method return ok

--- Benoit_Sorting_App/obj/Debug/net8.0/Benoit_Sorting_App.AssemblyInfo.cs
+++ Benoit_Sorting_App/obj/Debug/net8.0/Benoit_Sorting_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Benoit_Sorting_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eea20383e1a4df3239808b8f9e36fda0b3106cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a06b63a1eebde5711c33326753bbba5fe06fd29a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Benoit_Sorting_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Benoit_Sorting_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
