7913f78a71b5133f1e83117f64c1f80d32965ab4
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-12-06 16:08:26 +0100

    HttpGet to get all data for one player

--- Benoit_Sorting_App/obj/Debug/net8.0/Benoit_Sorting_App.AssemblyInfo.cs
+++ Benoit_Sorting_App/obj/Debug/net8.0/Benoit_Sorting_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Benoit_Sorting_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80c207f119a6e53553aca6fdcdba31bf8efc52c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2adb25913d613edd8ebbdbb171ec7066c69623a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Benoit_Sorting_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Benoit_Sorting_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
