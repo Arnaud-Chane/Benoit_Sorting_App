4c14cd1b7203d642ee501ec5a88a7f61b19b51bd
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-12-12 15:01:20 +0100

    Adding the player arr for tests

--- Benoit_Sorting_App/Services/PlayerService/PlayerService.cs
+++ Benoit_Sorting_App/Services/PlayerService/PlayerService.cs
@@ -1,7 +1,18 @@
-﻿namespace Benoit_Sorting_App.Services.PlayerService
+﻿using benoit_Sorting_App.Models;
+
+namespace Benoit_Sorting_App.Services.PlayerService
 {
     public class PlayerService : IPlayerService
     {
+        private static List<Player> players = new List<Player>
+        {
+            new Player
+            {
+                PlayerAlias = "Test",
+                TournamentPlace = 34
+            }
+        };
+
 
     }
 }
